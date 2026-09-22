860376af627fd4ce9941e17bd5d8f915e9f5e1b8
mape0s <mauricio2007xalves@gmail.com>
2026-09-21 23:29:48 -0300

    Restringe bloqueio de contas ao gerente responsavel

--- app/Http/Controllers/ContaController.php
+++ app/Http/Controllers/ContaController.php
@@ -2,28 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Conta;
 use App\Services\ContaService;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 
 class ContaController extends Controller
 {
     public function __construct(protected ContaService $service) {}
-
-    public function index()
-    {
-        if (Auth::user()->role_id !== 2) {
-            abort(403);
-        }
-
-        $contas = Conta::with('cliente')
-            ->whereNotNull('user_id')
-            ->orderBy('id')
-            ->get();
-
-        return view('contas.index', compact('contas'));
-    }
 
     public function bloquear(string $id)
     {
--- app/Policies/ContaPolicy.php
+++ app/Policies/ContaPolicy.php
@@ -9,11 +9,13 @@ class ContaPolicy
 {
     public function bloquear(User $user, Conta $conta): bool
     {
-        return $user->role_id === 2;
+        return $user->role_id === 2
+            && (int) $conta->gerente_conta_id === (int) $user->id;
     }
 
     public function desbloquear(User $user, Conta $conta): bool
     {
-        return $user->role_id === 2;
+        return $user->role_id === 2
+            && (int) $conta->gerente_conta_id === (int) $user->id;
     }
 }
